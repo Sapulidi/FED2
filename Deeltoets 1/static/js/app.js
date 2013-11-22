493bf940d381f809f206a671c2c3a42f7498ebb5
Sapulidi <sapulidi_@hotmail.com>
2013-11-22 21:32:58 +0100

    Removed tables and changed lay-out

--- Deeltoets 1/static/js/app.js
+++ Deeltoets 1/static/js/app.js
@@ -13,8 +13,8 @@ var FED_APP = FED_APP || {};
 	// Als de dom ready is wordt de init methode binnen heb routing object aangeroepen
 	FED_APP.init = function (){
 		FED_APP.routing.init();
-		FED_APP.gestures.registerSwipeLeft();
-		FED_APP.gestures.registerSwipeRight();
+		//FED_APP.gestures.registerSwipeLeft();
+		//FED_APP.gestures.registerSwipeRight();
 	}  
 
 	// Het post object
@@ -54,6 +54,8 @@ var FED_APP = FED_APP || {};
 
 			    'schedule': function() { // Als routie 'schedule' in de url tegen komt wordt showSchedulPage() aangeroepen
 			    	FED_APP.pages.showSchedulePage();
+			    	FED_APP.toggle.isSelected('j-scheduleanchor', true);
+			    	FED_APP.toggle.isSelected('j-rankinganchor', false);
 			    },
 
 				'updategame/:id': function(id) { 	// Als routie 'updategame/:id' in de url tegen komt wordt showUpdateGamePage() aangeroepen
@@ -64,6 +66,8 @@ var FED_APP = FED_APP || {};
 			    'ranking, *': function() { 	// Als routie 'ranking' in de url tegen komt wordt showrankingpage aangeroepen. Als er niets in de
 			    							// url staat wordt deze functie ook aangeroepen.
 			    	FED_APP.pages.showRankingPage();
+			    	FED_APP.toggle.isSelected('j-rankinganchor', true);
+			    	FED_APP.toggle.isSelected('j-scheduleanchor', false);
 			    }
 
 			});
@@ -73,7 +77,7 @@ var FED_APP = FED_APP || {};
 	FED_APP.gestures = {
 		registerSwipeLeft : function (){
 			var element = document.getElementById('body');
-    		var hammertime = Hammer(element).on("dragleft", function(event) {
+   		var hammertime = Hammer(element).on("dragleft", function(event) {
     			FED_APP.pages.showSchedulePage();
     		});
 		},
@@ -94,18 +98,16 @@ var FED_APP = FED_APP || {};
 			} else {
 				e.className = e.className.replace('show ', '');
 			}
-		}
-
-		// !!!!!! nu nog een fix vinden om de initial states van elementen te fixen (omdat inline css nu niet aanwezig in de css).
+		},
 
 		isSelected : function (elementId, selected){
 			// opzetje voor de is-selected addClass functie voor menuitems
 			var e = document.getElementById(elementId);
 
 			if(selected) {
-				//
+				e.className = e.className.replace('', 'is-selected ');
 			} else {
-				//
+				e.className = e.className.replace('is-selected ', '');
 			}
 		}
 	}
@@ -113,10 +115,10 @@ var FED_APP = FED_APP || {};
 	FED_APP.loader = {
 
 		show : function (){
-			FED_APP.toggle.showHide('loader', true);
+			FED_APP.toggle.showHide('j-loader', true);
 		},
 		hide : function (){
-			FED_APP.toggle.showHide('loader', false);
+			FED_APP.toggle.showHide('j-loader', false);
 		}
 	}
 
@@ -126,7 +128,7 @@ var FED_APP = FED_APP || {};
 			FED_APP.pages.hideAllPages();
 
 			FED_APP.data.getScheduleData( function(scheduleData) {
-				(document.getElementById('scheduleData')).style.display = 'block' ;
+				FED_APP.toggle.showHide('j-scheduleData', true);
 
 				var directives = {
 					id: {
@@ -145,7 +147,7 @@ var FED_APP = FED_APP || {};
 					}
 				};
 
-				Transparency.render(document.getElementById('scheduleTable'), scheduleData, directives);
+				Transparency.render(document.getElementById('j-scheduleTable'), scheduleData, directives);
 				FED_APP.loader.hide();
 			});
 
@@ -156,8 +158,8 @@ var FED_APP = FED_APP || {};
 			FED_APP.pages.hideAllPages();	
 
 			FED_APP.data.getGameData( function(rankingData) {
-				(document.getElementById('rankingData')).style.display = 'block' ;
-				Transparency.render(document.getElementById('rankingData'), rankingData);
+				FED_APP.toggle.showHide('j-rankingData', true);
+				Transparency.render(document.getElementById('j-rankingData'), rankingData);
 				FED_APP.loader.hide();
 			});
 		},
@@ -166,16 +168,16 @@ var FED_APP = FED_APP || {};
 			FED_APP.pages.hideAllPages();	
 
 			FED_APP.data.getGameScore( id, function(gameScore) {
-				(document.getElementById('updateGame')).style.display = 'block' ;
-				Transparency.render(document.getElementById('updateGame'), gameScore);
+				FED_APP.toggle.showHide('j-updateGame', true);
+				Transparency.render(document.getElementById('j-updateGame'), gameScore);
 				FED_APP.loader.hide();
 			});
 		},
 
 		hideAllPages : function() {
-			(document.getElementById('scheduleData')).style.display = 'none' ; // Verander de (inline) css naar display none
-			(document.getElementById('rankingData')).style.display = 'none' ;
-			(document.getElementById('updateGame')).style.display = 'none' ;
+			FED_APP.toggle.showHide('j-scheduleData', false);
+			FED_APP.toggle.showHide('j-rankingData', false);
+			FED_APP.toggle.showHide('j-updateGame', false);
 		}
 	}
 
